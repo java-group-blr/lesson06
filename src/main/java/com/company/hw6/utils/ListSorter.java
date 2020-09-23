2cf05e869475d4d4bed3d597b08b2e1a74f43317
mmalashchonak <mmalashchonak@gmail.com>
2020-09-23 17:16:29 +0300

    List sort done

---
+++ src/main/java/com/company/hw6/utils/ListSorter.java
@@ -0,0 +1,42 @@
+package com.company.hw6.utils;
+
+import com.company.hw6.bo.Person;
+
+import java.util.Collections;
+import java.util.List;
+
+public class ListSorter {
+
+    public List<Integer> sortIntegerFromMaxToMin(List<Integer> list) {
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size() - 1; j++) {
+                if (list.get(j + 1) > list.get(j)) {
+                    Integer temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+        return list;
+    }
+
+    public List<Person> sortPersonFromMaxToMinByComparable(List<Person> list) {
+
+        for (int i = 0; i < list.size(); i++) {
+            for (int j = 0; j < list.size() - 1; j++) {
+                if (list.get(j + 1).compareTo(list.get(j)) < 0) {
+                    Person temp = list.get(j);
+                    list.set(j, list.get(j + 1));
+                    list.set(j + 1, temp);
+                }
+            }
+        }
+        return list;
+    }
+
+    public List<Person> sortPersonFromMaxToMinByComparator(List<Person> list) {
+        Collections.sort(list, new PersonComparator());
+        return list;
+    }
+}
